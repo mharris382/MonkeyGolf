0f7eded4b1814471937bf37ed9aafa1809c54091
Max H <mharris382@gmail.com>
2026-03-01 16:49:47 -0500

    setup C++ modules and submoduled vre

---
+++ VRMonkeyGolf/Source/VRMonkeyGolf/Public/Core/MonkeyGolfGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MonkeyGolfGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRMONKEYGOLF_API AMonkeyGolfGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
